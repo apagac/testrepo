9206e63ef34f84cbf354b98be5ba4a6a304d26d9
Antonin Pagac <apagac@redhat.com>
2013-01-25 14:04:03 +0100

    Another try, changed test

--- trythisyounaughtyboy.py
+++ trythisyounaughtyboy.py
@@ -2,3 +2,6 @@
 
 #just try to write something
 print ("Hello, I'm having a wonderful day! And you cannot do anything to ruin it, HAHA!")
+
+#what about this
+print "Maybe I won't ruin your day, but I can still try"
